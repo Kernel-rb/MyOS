4b1dbcdae422a12c8a32122805e7d45e7d9996f3
Kernel-rb <matabsaifeddine@gmail.com>
2024-02-03 17:34:11 +0100

    Update Cargo.toml and main.rs files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-#![no_std]
+#![no_std]  // to disable the standard library
+#![no_main] // to disable entry point of the program
 
-fn main() {}
+use core::panic::PanicInfo;
+
+#[no_mangle] // mt encodich lia had l function name
+pub extern "C" fn _start() -> ! {
+    // the default entry point of the program && also is the entry point of every OS
+    // "C" => to tell the compiler to use the C calling convention
+    // "!": the function never returns 
+    loop {}
+}
+
+
+// #[cfg(not(test))]
+#[panic_handler] // when the program panics, it will call this function
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+
+
