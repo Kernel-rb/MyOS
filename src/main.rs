cd11f7740e7518cbed615258f8b133882351b5a4
Kernel-rb <matabsaifeddine@gmail.com>
2024-02-04 12:50:08 +0100

    fix bug on kernel

--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,18 @@ fn panic(_info: &PanicInfo) -> ! {
 }
 
 
-static WELCOME: &[u8] = b"I'm Alive!"; // declare a static variable that contains the welcome message
+static WELCOME: &[u8] = b"I'm Alive";// declare a static variable that contains the welcome message
 
 #[no_mangle] // mt encodich lia had l function name
 pub extern "C" fn _start() -> ! {
     // the default entry point of the program && also is the entry point of every OS
     // "C" => to tell the compiler to use the C calling convention
     // "!": the function never returns 
-    // the address of the VGA buffer , vga buffer est un tableau de 80*25 qui contient les caractères à afficher
-    let vga_buffer = 0xb8000 as *mut u8; // 0xb8000 is the address of the VGA buffer in memory
-    for ( i , &byte ) in WELCOME.iter().enumerate() { // enumerate() returns a tuple (index, value)
+    let vga_buffer = 0xb8000 as *mut u8; // the address of the VGA buffer , vga buffer est un tableau de 80*25 qui contient les caractères à afficher
+    for (i, &byte) in WELCOME.iter().enumerate() { // iterate over the welcome message
         unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte; // the first byte is the character
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb; // the second byte is the color ( 0xb is light cyan)
+            *vga_buffer.offset(i as isize * 2) = byte; // write the byte to the VGA buffer
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb; // set the color of the byte
         }
     }
     loop {}
