4e2dc634f2c6e8117a9c795fd0470d9d880182e0
Kernel-rb <matabsaifeddine@gmail.com>
2024-03-04 16:48:12 +0100

    showing lvl 4 and lvl 3 page tables

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     // "!": the function never returns 
     use my_os::memory::active_level_4_table;
     use x86_64::VirtAddr;
+    use x86_64::structures::paging::PageTable;
     // welcome message
     println!("Welcome to  RustOS , version: {}", "0.1.0");
     my_os::init(); // to initialize the OS
@@ -27,6 +28,19 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     for (i, entry) in l4_table.iter().enumerate() {
         if !entry.is_unused() {
             println!("L4 Entry {}: {:?}", i, entry);
+        
+            // get the physical address from the entry and convert it
+            let phys = entry.frame().unwrap().start_address();
+            let virt = phys.as_u64() + boot_info.physical_memory_offset;
+            let ptr = VirtAddr::new(virt).as_mut_ptr();
+            let l3_table: &PageTable = unsafe { &*ptr };
+        
+            // print non-empty entries of the level 3 table
+            for (i, entry) in l3_table.iter().enumerate() {
+                if !entry.is_unused() {
+                    println!("  L3 Entry {}: {:?}", i, entry);
+                }
+            }
         }
     }
     #[cfg(test)]
