ab39098eaab2f025fbb7532ce71a25d92f1c6c4a
Kernel-rb <matabsaifeddine@gmail.com>
2024-03-04 17:06:06 +0100

    Refactor memory translation and update main.rs

--- src/main.rs
+++ src/main.rs
@@ -16,43 +16,23 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     // the default entry point of the program && also is the entry point of every OS
     // "C" => to tell the compiler to use the C calling convention
     // "!": the function never returns 
-    use my_os::memory::active_level_4_table;
+    use my_os::memory::translate_addr;
     use x86_64::VirtAddr;
-    use x86_64::structures::paging::PageTable;
     // welcome message
     println!("Welcome to  RustOS , version: {}", "0.1.0");
     my_os::init(); // to initialize the OS
-    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset); // to get the physical memory offset
-    let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
-
-    for (i, entry) in l4_table.iter().enumerate() {
-        if !entry.is_unused() {
-            println!("L4 Entry {}: {:?}", i, entry);
-        
-            // get the physical address from the entry and convert it
-            let phys = entry.frame().unwrap().start_address();
-            let virt = phys.as_u64() + boot_info.physical_memory_offset;
-            let ptr = VirtAddr::new(virt).as_mut_ptr();
-            let l3_table: &PageTable = unsafe { &*ptr };
-        
-            // print non-empty entries of the level 3 table
-            for (i, entry) in l3_table.iter().enumerate() {
-                if !entry.is_unused() {
-                    println!("  L3 Entry {}: {:?}", i, entry);
-                }
-            }
-        }
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let addresses = [0xb8000,0x201008,0x0100_0020_1a10,boot_info.physical_memory_offset];
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
+        println!("{:?} -> {:?}", virt, phys);
     }
     #[cfg(test)]
-    test_main();
-
-    
+    test_main();  
     println!("It did not crash!");
     my_os::hlt_loop();
 }
-
-
-
 // -------------------------------------- Panic Handler  --------------------------------------
 #[cfg(not(test))]
 #[panic_handler]
@@ -60,12 +40,8 @@ fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     my_os::hlt_loop();            // new
 }
-
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     my_os::test_panic_handler(info)
 }
-
-
-
