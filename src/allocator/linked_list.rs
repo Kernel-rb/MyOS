6da12cdbaaa4ff3991869bb025fc827dd5699b92
Kernel-rb <matabsaifeddine@gmail.com>
2024-03-06 22:08:42 +0100

    Add LinkedListAllocator struct and initialization method

--- src/allocator/linked_list.rs
+++ src/allocator/linked_list.rs
@@ -11,3 +11,30 @@ impl ListNode {
         self.start_addr() + self.size
     }
 }
+
+pub struct LinkedListAllocator {
+    head: ListNode,
+}
+
+impl LinkedListAllocator {
+    /// Creates an empty LinkedListAllocator.
+    pub const fn new() -> Self {
+        Self {
+            head: ListNode::new(0),
+        }
+    }
+
+    /// Initialize the allocator with the given heap bounds.
+    ///
+    /// This function is unsafe because the caller must guarantee that the given
+    /// heap bounds are valid and that the heap is unused. This method must be
+    /// called only once.
+    pub unsafe fn init(&mut self, heap_start: usize, heap_size: usize) {
+        self.add_free_region(heap_start, heap_size);
+    }
+
+    /// Adds the given memory region to the front of the list.
+    unsafe fn add_free_region(&mut self, addr: usize, size: usize) {
+        todo!();
+    }
+}
