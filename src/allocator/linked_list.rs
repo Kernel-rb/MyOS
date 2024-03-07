48dc7b377f39b36bd8b34343105609da7e92f5ef
Kernel-rb <matabsaifeddine@gmail.com>
2024-03-07 10:53:00 +0100

    Add LinkedListAllocator implementation and methods

--- src/allocator/linked_list.rs
+++ src/allocator/linked_list.rs
@@ -1,3 +1,12 @@
+use super::align_up;
+use core::mem;
+
+
+struct ListNode {
+    size: usize,
+    next: Option<&'static mut ListNode>,
+}
+
 impl ListNode {
     const fn new(size: usize) -> Self {
         ListNode { size, next: None }
@@ -17,24 +26,41 @@ pub struct LinkedListAllocator {
 }
 
 impl LinkedListAllocator {
-    /// Creates an empty LinkedListAllocator.
     pub const fn new() -> Self {
         Self {
             head: ListNode::new(0),
         }
     }
-
-    /// Initialize the allocator with the given heap bounds.
-    ///
-    /// This function is unsafe because the caller must guarantee that the given
-    /// heap bounds are valid and that the heap is unused. This method must be
-    /// called only once.
     pub unsafe fn init(&mut self, heap_start: usize, heap_size: usize) {
         self.add_free_region(heap_start, heap_size);
     }
-
-    /// Adds the given memory region to the front of the list.
     unsafe fn add_free_region(&mut self, addr: usize, size: usize) {
-        todo!();
+        assert_eq!(align_up(addr, mem::align_of::<ListNode>()), addr);
+        assert!(size >= mem::size_of::<ListNode>());
+        let mut node = ListNode::new(size);
+        node.next = self.head.next.take();
+        let node_ptr = addr as *mut ListNode;
+        node_ptr.write(node);
+        self.head.next = Some(&mut *node_ptr)
+    }
+    fn find_region(&mut self, size: usize, align: usize)
+    -> Option<(&'static mut ListNode, usize)>
+    {
+
+        let mut current = &mut self.head;
+        // look for a large enough memory region in linked list
+        while let Some(ref mut region) = current.next {
+            if let Ok(alloc_start) = Self::alloc_from_region(&region, size, align) {
+                // region suitable for allocation -> remove node from list
+                let next = region.next.take();
+                let ret = Some((current.next.take().unwrap(), alloc_start));
+                current.next = next;
+                return ret;
+            } else {
+                // region not suitable -> continue with next region
+                current = current.next.as_mut().unwrap();
+            }
+        }
+        None
     }
 }
