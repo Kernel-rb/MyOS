a91aa4b1e2e1d752817620aa656377b410181034
Kernel-rb <matabsaifeddine@gmail.com>
2024-03-07 11:04:35 +0100

    Update allocator to use LinkedListAllocator

--- src/allocator.rs
+++ src/allocator.rs
@@ -9,12 +9,13 @@ use x86_64::{
 use alloc::alloc::{GlobalAlloc, Layout};
 use core::ptr::null_mut;
 use bump::BumpAllocator;
-
+use linked_list::LinkedListAllocator;
 
 
 #[global_allocator]
-static ALLOCATOR: Locked<BumpAllocator> = Locked::new(BumpAllocator::new());
-
+static ALLOCATOR: Locked<LinkedListAllocator> =
+    Locked::new(LinkedListAllocator::new());
+    
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; 
 pub struct Locked<A> {
--- src/allocator/linked_list.rs
+++ src/allocator/linked_list.rs
@@ -1,10 +1,35 @@
-use super::align_up;
-use core::mem;
+use super::{align_up,Locked};
+use core::{mem,ptr};
+use alloc::alloc::{GlobalAlloc,Layout};
 
 
 struct ListNode {
     size: usize,
     next: Option<&'static mut ListNode>,
+}
+
+unsafe impl GlobalAlloc for Locked<LinkedListAllocator> {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        let (size, align) = LinkedListAllocator::size_align(layout);
+        let mut allocator = self.lock();
+
+        if let Some((region, alloc_start)) = allocator.find_region(size, align) {
+            let alloc_end = alloc_start.checked_add(size).expect("overflow");
+            let excess_size = region.end_addr() - alloc_end;
+            if excess_size > 0 {
+                allocator.add_free_region(alloc_end, excess_size);
+            }
+            alloc_start as *mut u8
+        } else {
+            ptr::null_mut()
+        }
+    }
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        let (size, _) = LinkedListAllocator::size_align(layout);
+
+        self.lock().add_free_region(ptr as usize, size)
+    }
+    
 }
 
 impl ListNode {
@@ -62,17 +87,25 @@ impl LinkedListAllocator {
     }
     fn alloc_from_region(region: &ListNode, size: usize, align: usize)
     -> Result<usize, ()>
-{
-    let alloc_start = align_up(region.start_addr(), align);
-    let alloc_end = alloc_start.checked_add(size).ok_or(())?;
+    {
+        let alloc_start = align_up(region.start_addr(), align);
+        let alloc_end = alloc_start.checked_add(size).ok_or(())?;
 
-    if alloc_end > region.end_addr() {
-        return Err(());
+        if alloc_end > region.end_addr() {
+            return Err(());
+        }
+        let excess_size = region.end_addr() - alloc_end;
+        if excess_size > 0 && excess_size < mem::size_of::<ListNode>() {
+            return Err(());
+        }
+        Ok(alloc_start)
     }
-    let excess_size = region.end_addr() - alloc_end;
-    if excess_size > 0 && excess_size < mem::size_of::<ListNode>() {
-        return Err(());
+    fn size_align(layout: Layout) -> (usize, usize) {
+        let layout = layout
+            .align_to(mem::align_of::<ListNode>())
+            .expect("adjusting alignment failed")
+            .pad_to_align();
+        let size = layout.size().max(mem::size_of::<ListNode>());
+        (size, layout.align())
     }
-    Ok(alloc_start)
-}
 }
