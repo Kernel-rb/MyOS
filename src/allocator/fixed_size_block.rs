7a7610113f47142d7b55255ecaf0bd0da1dfd4c8
Kernel-rb <matabsaifeddine@gmail.com>
2024-03-11 13:41:29 +0100

    Add FixedSizeBlockAllocator struct and implementation

---
+++ src/allocator/fixed_size_block.rs
@@ -0,0 +1,22 @@
+struct ListNode {
+    next: Option<&'static mut ListNode>,
+}
+
+const BLOCK_SIZES: &[usize] = &[8, 16, 32, 64, 128, 256, 512, 1024, 2048];
+pub struct FixedSizeBlockAllocator {
+    list_heads: [Option<&'static mut ListNode>; BLOCK_SIZES.len()],
+    fallback_allocator: linked_list_allocator::Heap,
+}
+
+impl FixedSizeBlockAllocator {
+    pub const fn new() -> Self {
+        const EMPTY: Option<&'static mut ListNode> = None;
+        FixedSizeBlockAllocator {
+            list_heads: [EMPTY; BLOCK_SIZES.len()],
+            fallback_allocator: linked_list_allocator::Heap::empty(),
+        }
+    }
+    pub unsafe fn init(&mut self, heap_start: usize, heap_size: usize) {
+        self.fallback_allocator.init(heap_start, heap_size);
+    }
+}
