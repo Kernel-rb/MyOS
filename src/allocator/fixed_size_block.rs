d36029e5e8547321ca80ef47b06dd8bc01132bc7
Kernel-rb <matabsaifeddine@gmail.com>
2024-03-11 13:42:15 +0100

    Add fallback_alloc method to FixedSizeBlockAllocator

--- src/allocator/fixed_size_block.rs
+++ src/allocator/fixed_size_block.rs
@@ -1,3 +1,6 @@
+use alloc::alloc::Layout;
+use core::ptr;
+
 struct ListNode {
     next: Option<&'static mut ListNode>,
 }
@@ -16,7 +19,14 @@ impl FixedSizeBlockAllocator {
             fallback_allocator: linked_list_allocator::Heap::empty(),
         }
     }
+    
     pub unsafe fn init(&mut self, heap_start: usize, heap_size: usize) {
         self.fallback_allocator.init(heap_start, heap_size);
+    }
+    fn fallback_alloc(&mut self, layout: Layout) -> *mut u8 {
+        match self.fallback_allocator.allocate_first_fit(layout) {
+            Ok(ptr) => ptr.as_ptr(),
+            Err(_) => ptr::null_mut(),
+        }
     }
 }
