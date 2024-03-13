f3051135c5c73872c997b74f8c0be206fc4b90c1
Kernel-rb <matabsaifeddine@gmail.com>
2024-03-13 15:57:23 +0100

    Add simple_executor module

--- src/task/mod.rs
+++ src/task/mod.rs
@@ -2,6 +2,12 @@ use core::{future::Future , pin::Pin};
 use alloc::boxed::Box;
 use core::task::{Context , Poll};
 
+
+pub mod simple_executor;
+
+
+
+
 pub struct Task {
     future: Pin<Box<dyn Future<Output = ()>>
 }
