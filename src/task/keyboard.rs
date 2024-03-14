7b9de8486a4e5f38176c471c6dd96d14fefb8279
Kernel-rb <matabsaifeddine@gmail.com>
2024-03-14 14:52:45 +0100

    Add futures-util crate and implement ScancodeStream

--- src/task/keyboard.rs
+++ src/task/keyboard.rs
@@ -1,17 +1,64 @@
 use conquer_once::spin::OnceCell;
 use crossbeam_queue::ArrayQueue;
+use core::{pin::Pin, task::{Poll, Context}};
+use futures_util::stream::Stream;
+use futures_util::task::AtomicWaker;
+
 use crate::println; 
 
 
+// The waker for the scancode stream
+static WAKER: AtomicWaker = AtomicWaker::new();
+
+pub struct ScancodeStream {
+    _private: (),
+}
+
+impl ScancodeStream {
+    pub fn new() -> Self {
+        SCANCODE_QUEUE.try_init_once(|| ArrayQueue::new(100))
+            .expect("ScancodeStream::new should only be called once");
+        ScancodeStream { _private: () }
+    }
+}
+
+impl Stream for ScancodeStream {
+    type Item = u8;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<u8>> {
+        let queue = SCANCODE_QUEUE
+            .try_get()
+            .expect("scancode queue not initialized");
+
+        if let Ok(scancode) = queue.pop() {
+            return Poll::Ready(Some(scancode));
+        }
+
+        WAKER.register(&cx.waker());
+        match queue.pop() {
+            Ok(scancode) => {
+                WAKER.take();
+                Poll::Ready(Some(scancode))
+            }
+            Err(crossbeam_queue::PopError) => Poll::Pending,
+        }
+    }
+}
+
+
+//------------------------------------------------------------------------------------------------
+// -------------------------------------- Keyboard Interrupt Handler  ----------------------------
+//------------------------------------------------------------------------------------------------
+
 // The size of the scancode queue 
 static SCANCODE_QUEUE: OnceCell<ArrayQueue<u8>> = OnceCell::uninit();
-
-
 // Called by the keyboard interrupt handler to add a scancode to the queue
 pub(crate) fn add_scancode(scancode: u8) {
     if let Ok(queue) = SCANCODE_QUEUE.try_get() {
         if let Err(_) = queue.push(scancode) {
             println!("WARNING: scancode queue full; dropping keyboard input");
+        } else {
+            WAKER.wake(); // new
         }
     } else {
         println!("WARNING: scancode queue uninitialized");
