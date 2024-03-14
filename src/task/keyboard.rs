2aa76a27c89acd9cb60af4ed06b93d7483eb3f70
Kernel-rb <matabsaifeddine@gmail.com>
2024-03-14 14:38:27 +0100

    Add scancode handling to keyboard module

--- src/task/keyboard.rs
+++ src/task/keyboard.rs
@@ -1,4 +1,15 @@
 use conquer_once::spin::OnceCell;
 use crossbeam_queue::ArrayQueue;
+use crate::println; 
 
 static SCANCODE_QUEUE: OnceCell<ArrayQueue<u8>> = OnceCell::uninit();
+
+pub(crate) fn add_scancode(scancode: u8) {
+    if let Ok(queue) = SCANCODE_QUEUE.try_get() {
+        if let Err(_) = queue.push(scancode) {
+            println!("WARNING: scancode queue full; dropping keyboard input");
+        }
+    } else {
+        println!("WARNING: scancode queue uninitialized");
+    }
+}
