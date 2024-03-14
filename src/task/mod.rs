4f4aade07b04077dd84754949acce2a5ded3e947
Kernel-rb <matabsaifeddine@gmail.com>
2024-03-14 14:28:25 +0100

    Add new constructor for Task struct ( error fixed)

--- src/task/mod.rs
+++ src/task/mod.rs
@@ -15,6 +15,11 @@ pub struct Task {
 }
 
 impl Task {
+    pub fn new(future: impl Future<Output = ()> + 'static) -> Task {
+        Task {
+            future: Box::pin(future),
+        }
+    }
     fn poll(&mut self, context: &mut Context) -> Poll<()> {
         self.future.as_mut().poll(context)
     }
