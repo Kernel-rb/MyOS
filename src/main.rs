26054b06467b345bc8e731feef5d843f90d5d9bc
Kernel-rb <matabsaifeddine@gmail.com>
2024-03-14 14:54:43 +0100

    Add keyboard task to executor

--- src/main.rs
+++ src/main.rs
@@ -32,6 +32,7 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     allocator::init_heap(&mut _mapper, &mut _frame_allocator).expect("heap initialization failed");
     let mut executor = SimpleExecutor::new();
     executor.spawn(Task::new(example_task()));
+    executor.spawn(Task::new(keyboard::print_keypresses()));
     executor.run();
 
 
