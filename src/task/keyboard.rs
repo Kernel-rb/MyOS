8282f1de72d9ef1a2fed55e54633d9627e8f979e
Kernel-rb <matabsaifeddine@gmail.com>
2024-03-14 14:41:52 +0100

    Add scancode to keyboard queue

--- src/task/keyboard.rs
+++ src/task/keyboard.rs
@@ -2,8 +2,12 @@ use conquer_once::spin::OnceCell;
 use crossbeam_queue::ArrayQueue;
 use crate::println; 
 
+
+// The size of the scancode queue 
 static SCANCODE_QUEUE: OnceCell<ArrayQueue<u8>> = OnceCell::uninit();
 
+
+// Called by the keyboard interrupt handler to add a scancode to the queue
 pub(crate) fn add_scancode(scancode: u8) {
     if let Ok(queue) = SCANCODE_QUEUE.try_get() {
         if let Err(_) = queue.push(scancode) {
